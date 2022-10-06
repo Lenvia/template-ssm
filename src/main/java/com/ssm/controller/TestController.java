29dc1c156a99785fe5317fef39cbea175bab9fac
Lenvia <yrp1199@163.com>
2022-10-07 02:28:48 +0800

    fix template

--- src/main/java/com/ssm/controller/TestController.java
+++ src/main/java/com/ssm/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.ssm.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * FileName: TestController
  * Author: Runpu
- * Date: 2022/10/322:22s
+ * Date: 2022/10/3 22:22s
  */
 @Controller
 public class TestController {
+
+    @RequestMapping("/hello")
+    public String Hello(){
+        return "success";
+    }
 }
